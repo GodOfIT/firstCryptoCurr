68ba8206be28468036769c34646b7cfc172844cc
GodOfIT <ducbmit@gmail.com>
2018-03-28 12:55:05 +0700

    up code

---
+++ BitCoin/Program.cs
@@ -0,0 +1,25 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Key privateKey = new Key();
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(privateKey);
+            Console.WriteLine(publicKey);
+            Console.WriteLine(publicKey.GetAddress(Network.Main).ToString().Length);
+            Console.WriteLine(publicKey.GetAddress(Network.TestNet));
+            Console.ReadKey();
+
+        }
+    }
+}
